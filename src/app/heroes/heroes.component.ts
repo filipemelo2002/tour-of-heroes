7df3e978837d3a14f85eb4460131a61215db6857
Filipe Melo <filipe.melo@liferay.com>
2021-06-22 16:13:57 -0300

    Properly calling Heroes and Message Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,15 +7,21 @@ import {HEROES} from '../mock-heroes'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[] = []
   selectedHero?: Hero;
-  constructor() { }
+  constructor(private heroService: HeroService,  private messageService: MessageService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes=> this.heroes = heroes);
   }
 
 }
